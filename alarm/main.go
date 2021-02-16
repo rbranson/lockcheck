151ac056bdf984e1251a848b4b01ed48002f8f41
Rick Branson <rick@diodeware.com>
2021-02-16 14:26:33 -0800

    nuke go's SIGALRM handler

--- alarm/main.go
+++ alarm/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"syscall"
 	"time"
+	"unsafe"
 )
+
+const SIG_DFL = 0
 
 func alarm(t int) error {
 	_, _, errno := syscall.Syscall(syscall.SYS_ALARM, uintptr(t), 0, 0)
@@ -14,7 +18,36 @@ func alarm(t int) error {
 	return nil
 }
 
+type sigaction_t struct {
+	handler uintptr
+	sigaction uintptr
+	mask uintptr
+	flags uintptr
+	restorer uintptr
+}
+
+func sigaction(sig syscall.Signal, sa sigaction_t) error {
+	_, _, errno := syscall.Syscall6(
+		syscall.SYS_RT_SIGACTION,
+		uintptr(sig),
+		uintptr(unsafe.Pointer(&sa)),
+		0,
+		unsafe.Sizeof(uintptr(0)),
+		0,
+		0)
+	if errno != 0 {
+		return errno
+	}
+	return nil
+}
+
 func main() {
+	// set the SIGALRM handler back to default
+	err := sigaction(syscall.SIGALRM, sigaction_t{handler: SIG_DFL})
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	for {
 		fmt.Printf("setting alarm...\n")
 		err := alarm(2)
